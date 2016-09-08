4aa7b881ede09d677024c69ff32f5167d776077f
kliprift <eddie@worldone.com.my>
2016-09-09 01:23:30 +0800

    created and passed the Post model with TDD, to be refactored furthered

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,37 @@
 class PostsController < ApplicationController
+  before_action :find_id, except: [:index, :new, :show, :create]
 
   def index
   end
+
+  def show
+    @post = Post.last
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
   
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def find_id
+    @post = Post.find(params[:id])
+  end
+
 end
